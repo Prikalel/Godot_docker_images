3952ccf3ef19946ae01def3e9b5ce752b2180b69
Alex Poklonsky <jispar@mail.ru>
2024-02-25 13:58:31 +0300

    add 4.2-stable-dotnet-8 with readme

---
+++ build/README.md
@@ -0,0 +1,5 @@
+# To build image
+
+## Download zip-ed godot engine (linux+net) and move it to here (replace empty `Godot_v4.2-stable_mono_linux_x86_64.zip` with it)
+
+## Download export templates for 4.2 stable export templates and extract them to here (file `templates/version.txt` will be replaced)
---
+++ build/check_checksum.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]
+then
+  echo "No arguments supplied"
+  exit 1
+fi
+
+CHECKSUMS_FILE="SHA512-SUMS.txt"
+GODOT_ZIP_FILE=$1
+
+if [ -f "$CHECKSUMS_FILE" ]
+then
+  echo "File $CHECKSUMS_FILE exists, but must be downloaded"
+  exit 2
+else
+  echo "File $CHECKSUMS_FILE does not exist and will be downloaded..."
+  wget https://downloads.tuxfamily.org/godotengine/4.2/mono/$CHECKSUMS_FILE
+fi
+
+ACTUAL_SHA512=$(sha512sum $GODOT_ZIP_FILE)
+LOADED_SHA512=$(cat $CHECKSUMS_FILE | grep $GODOT_ZIP_FILE)
+
+if [ "$LOADED_SHA512" = "$ACTUAL_SHA512" ]
+then
+  echo "all OK"
+else
+  echo -e "no match, Local:"$LOADED_SHA512"\nRemote:"$ACTUAL_SHA512
+  exit 3
+fi
+
+rm -f $CHECKSUMS_FILE
